ac647fafbb620b24e782bba633641b2798133bb1
adeptex <arteaocho@gmail.com>
2015-05-24 00:29:08 +0200

    Create a5-msf_readfile.c

---
+++ Assignment-5/a5-msf_readfile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+/*
+ * linux/x86/read_file - 79 bytes
+ * http://www.metasploit.com
+ * VERBOSE=false, PrependFork=false, PrependSetresuid=false, 
+ * PrependSetreuid=false, PrependSetuid=false, 
+ * PrependSetresgid=false, PrependSetregid=false, 
+ * PrependSetgid=false, PrependChrootBreak=false, 
+ * AppendExit=false, PATH=/etc/resolve.conf, FD=1
+ */
+unsigned char code[] = \
+"\xeb\x36\xb8\x05\x00\x00\x00\x5b\x31\xc9\xcd\x80\x89\xc3\xb8"
+"\x03\x00\x00\x00\x89\xe7\x89\xf9\xba\x00\x10\x00\x00\xcd\x80"
+"\x89\xc2\xb8\x04\x00\x00\x00\xbb\x01\x00\x00\x00\xcd\x80\xb8"
+"\x01\x00\x00\x00\xbb\x00\x00\x00\x00\xcd\x80\xe8\xc5\xff\xff"
+"\xff\x2f\x65\x74\x63\x2f\x72\x65\x73\x6f\x6c\x76\x65\x2e\x63"
+"\x6f\x6e\x66\x00";
+
+int main()
+{
+	printf("Shellcode Length:  %d\n", strlen(code));
+	int (*ret)() = (int(*)())code;
+	ret();
+}
