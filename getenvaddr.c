cc4408b6bba29ddb095079ea3a79db9533d801b0
adeptex <adeptex@users.noreply.github.com>
2015-08-30 23:15:38 +0200

    Create getenvaddr.c

---
+++ getenvaddr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int arc, char* argv[]) {
+	char *p;
+	
+	if (argc < 3) {
+		printf("Usage: %s <env var> <binary>\n", argv[0]);
+		exit(0);
+	}
+	
+	p = getenv(argv[1]);
+	p += (strlen(argv[0]) - strlen(argv[2]))*2;
+	printf("%s will be at %p", argv[1], p);
+}
